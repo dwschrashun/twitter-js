a6b0f461971a75d4dcb191c05c7261eaf40dad48
Steve spells <spells24@gmail.com>
2015-08-05 14:05:18 -0400

    templates finished

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require( 'express' );
+var swig = require('swig');
+
 var app = express(); // creates an instance of an express application
 
 var server = app.listen(3000);
 console.log("server listening");
+
+// var mySwig = new swig.Swig({varControls: ["{{","}}"], tagControls: ["{%", "%}"]});
+var rendered = swig.renderFile(__dirname + '/views/index.html',
+	{title: "An Example", 
+	 people: [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]
+	},  
+
+	function(err,output){
+	if (err)
+		console.error(err);
+	else
+		console.log(output);
+})
+
 
 app.use(function (request, response, next) {
 	console.log(request.method, request.path);
