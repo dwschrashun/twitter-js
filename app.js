d3c0e518dafc93a356f1bd022f293e495cdaa68b
dwschrashun <dwschrashun@gmail.com>
2015-08-05 14:57:16 -0400

    added express template rendering engine

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ var server = app.listen(3000);
 console.log("server listening");
 
 // var mySwig = new swig.Swig({varControls: ["{{","}}"], tagControls: ["{%", "%}"]});
-var rendered = swig.renderFile(__dirname + '/views/index.html',
-	{title: "An Example", 
-	 people: [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]
-	},  
+// var rendered = swig.renderFile(__dirname + '/views/index.html',
+// 	{title: "An Example", 
+// 	 people: [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]
+// 	},  
 
-	function(err,output){
-	if (err)
-		console.error(err);
-	else
-		console.log(output);
-})
+// 	function(err,output){
+// 	if (err)
+// 		console.error(err);
+// 	else
+// 		console.log(output);
+// });
+
+app.engine("html", swig.renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname + '/views/');
+swig.setDefaults({cache:false});
 
 
 app.use(function (request, response, next) {
@@ -30,7 +35,8 @@ app.use(function (request, response, next) {
 
 app.get("/", function (request, response, next) {
 	console.log(response.statusCode);
-	response.send("Hello World");
+
+	var renderedHTML = response.render("index", {title: "An Example", people: [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]});
 });
 
 app.get("/news", function (request, response, next) {
